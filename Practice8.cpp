48312666c13f851c79d761f6280bb0aa1d6a0e48
Jaguar122 <146303431+Jaguar122@users.noreply.github.com>
2023-09-28 10:42:29 +0800

    Add files via upload

---
+++ Practice8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	string name;
+	
+	cout<<"Enter your name:";
+	getline(cin,name);//para sa input
+	cout<<"The length of your name is:"<<name.size();
+	
+	
+	
+	
+	
+	
+     
+	
+	return 0;
+}
