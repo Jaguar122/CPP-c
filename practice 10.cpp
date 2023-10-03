726c601a11793d5c1fa20af85424d8bb3d0890e5
Jaguar122 <146303431+Jaguar122@users.noreply.github.com>
2023-10-03 10:34:25 +0800

    Add files via upload

---
+++ practice 10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	
+	int grade;
+	
+	cout<<"Enter your grade: ";
+	cin>>grade;
+	
+	if(grade >= 75){
+		cout<<"Pasado";
+		
+			
+	}else{
+		cout<<" Bagsak";
+		
+	}
+	
+	return 0;
+}
