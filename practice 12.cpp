b02faf983916f2d998d47bea79d403d3382e3388
Jaguar122 <146303431+Jaguar122@users.noreply.github.com>
2023-10-03 10:35:05 +0800

    Add files via upload

---
+++ practice 12.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	
+	int num;
+	
+	cout<<"Enter your Number: ";
+	cin>>num;
+	
+	if(num % 2 ==0){
+		cout<<"Even Number";
+		
+			
+	}else{
+		cout<<"Odd Number";
+		
+	}
+	
+	return 0;
+}
