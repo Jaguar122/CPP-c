6bf593111250d72b251d831d8b9c2a2d479760d2
Jaguar122 <146303431+Jaguar122@users.noreply.github.com>
2023-11-14 10:10:09 +0800

    Add files via upload

---
+++ Practice18.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<chrono>
+#include<thread>
+
+using namespace std;
+
+int main(){
+	
+	int x = 0;
+	while (x<=50){
+		
+		cout<<"\nJay-R" <<x;
+		x++;
+		this_thread::sleep_for(chrono::milliseconds(100));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+return 0;
+}
