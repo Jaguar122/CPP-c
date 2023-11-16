544fde01554058a5ac4d07826ca9957eff23a9fb
Jaguar122 <146303431+Jaguar122@users.noreply.github.com>
2023-11-16 10:33:25 +0800

    Add files via upload

---
+++ Practice 20.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	string myName = "Jay-R Viray";
+	int size = myName.size();
+	for (int x=0; x < size; x++){
+		cout<<myName[x]<<"\n";
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
